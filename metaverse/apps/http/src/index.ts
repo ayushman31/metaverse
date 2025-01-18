c755783c65f3dc7fc40c862f7b6d34945f89cdef
Ayushman Singh <ayushmans012@gmail.com>
2025-01-19 04:03:11 +0530

    added database

--- metaverse/apps/http/src/index.ts
+++ metaverse/apps/http/src/index.ts
@@ -3,6 +3,7 @@ import { router } from "./routes/v1";
 import { userRouter } from "./routes/v1/user";
 import { adminRouter } from "./routes/v1/admin";
 import { spaceRouter } from "./routes/v1/space";
+import client from "@repo/db/client";
 
 const app = express();
 
